4ea9f7f657629674e475128a547fbc05f133ccab
Chicloon <Chicloon@gmail.com>
2018-02-08 15:32:06 +0900

    switched to semantic-react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import 'semantic-ui-css/semantic.min.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Test from './components/Test';
+import LoginFrom from './containers/LoginForm';
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Test} />
+          <Route exact path="/login" component={LoginFrom} />
         </Switch>
       </Router>
     );
